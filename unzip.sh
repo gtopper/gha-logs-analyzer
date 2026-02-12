b72315ca91f200d16e4b6d26cf1e39af88516872
Gal Topper <Gal_Topper-BNKK@mckinsey.com>
2026-02-12 13:25:46 +0700

    Fix unzip script

--- unzip.sh
+++ unzip.sh
@@ -7,11 +7,13 @@ fi
 
 mkdir -p "logs/$BRANCH"
 for path in log_archives/$BRANCH/*.zip; do
-  echo Unpacking "$path"
-  filename=$(basename "$path")
   # if file is nonempty (we create an empty file when a download fails, to prevent further attempts)
-  if [ -s "$filename" ]; then
+  if [ -s "$path" ]; then
+    echo Unpacking "$path"
+    filename=$(basename "$path")
     # -o to always overwrite, -n to never overwrite
     unzip -n "$path" -d "logs/$BRANCH/${filename%.*}";
+  else
+    echo Skipping empty file "$path"
   fi
 done
